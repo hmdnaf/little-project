79c9f1a960c92f27a39cc10b5f2c66126aa57c71
Hamdan AF <110399823+hmdnaf@users.noreply.github.com>
2024-06-27 11:19:21 +0800

    about section,menu section,

--- project-kedai-kopi/js/script.js
+++ project-kedai-kopi/js/script.js
@@ -1,15 +1,15 @@
 //toggle clss active
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 //ketika hamburger menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
-    navbarNav.classList.toggle('active');
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
-//klik dilur sidebar untuk menhilangkan nav/menunya
-const hamburger = document.querySelector('#hamburger-menu');
+//klik diluar sidebar untuk menhilangkan nav/menunya
+const hamburger = document.querySelector("#hamburger-menu");
 
-document.addEventListener('click', function (e) {
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
 });
